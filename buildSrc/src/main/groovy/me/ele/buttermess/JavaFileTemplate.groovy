fbb34d268a9b5e004dfb8bf4a9d1fbe6e9e7d835
peacepassion <daqi1990@gmail.com>
2016-06-23 13:08:06 +0800

    fix key words compile error

--- buildSrc/src/main/groovy/me/ele/buttermess/JavaFileTemplate.groovy
+++ buildSrc/src/main/groovy/me/ele/buttermess/JavaFileTemplate.groovy
@@ -2,6 +2,55 @@ package me.ele.buttermess
 
 class JavaFileTemplate {
 
+  String[] keyWords = ["do",
+                       "if",
+                       "for",
+                       "int",
+                       "new",
+                       "try",
+                       "byte",
+                       "case",
+                       "char",
+                       "else",
+                       "goto",
+                       "long",
+                       "this",
+                       "void",
+                       "break",
+                       "catch",
+                       "class",
+                       "const",
+                       "final",
+                       "float",
+                       "short",
+                       "super",
+                       "throw",
+                       "while",
+                       "double",
+                       "import",
+                       "native",
+                       "public",
+                       "return",
+                       "static",
+                       "switch",
+                       "throws",
+                       "boolean",
+                       "default",
+                       "extends",
+                       "finally",
+                       "package",
+                       "private",
+                       "abstract",
+                       "continue",
+                       "strictfp",
+                       "volatile",
+                       "interface",
+                       "protected",
+                       "transient",
+                       "implements",
+                       "instanceof",
+                       "synchronized"]
+
   String packageName
   Map<String, String> map;
 
@@ -10,10 +59,23 @@ class JavaFileTemplate {
     map.each { entry ->
       String injectedClazz = entry.key
       String viewInjectorClazz = entry.value
-      code += String.format("map.put(Class.forName(\"%s\"), new %s());\n", injectedClazz,
-          viewInjectorClazz)
+      // key must use reflection because there are some private or protected class
+      String key = "Class.forName(\"${injectedClazz}\")"
+      String value = containsKeyWord(viewInjectorClazz) ?
+          "Class.forName(\"${viewInjectorClazz}\").newInstance()" : "${viewInjectorClazz}()"
+      code += String.format("map.put(%s, new %s);\n", key, value)
     }
     return code
+  }
+
+  boolean containsKeyWord(String clazz) {
+    String[] names = clazz.split("\\.")
+    for (String name : names) {
+      if (keyWords.contains(name)) {
+        return true
+      }
+    }
+    return false
   }
 
   def getContent() {
